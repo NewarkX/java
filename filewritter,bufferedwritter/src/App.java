ad2ef5a47feb5c58ecb3002d5455c00085fbcd58
NewarkX <brunowmanha@gmail.com>
2022-02-24 10:27:34 -0300

    bufferedwritter,filewritter ok

--- filewritter,bufferedwritter/src/App.java
+++ filewritter,bufferedwritter/src/App.java
@@ -1,5 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        String[] lines = new String[] {"good morning","good afternoon","good night"};
+
+        String path = "C:\\Users\\User\\Desktop\\java\\filewritter,bufferedwritter\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines) { //para cada string line contido no vetor lines
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
